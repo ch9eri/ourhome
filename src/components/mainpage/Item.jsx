d1ad14bc3c9421ed03a5024036c23ac81d2ff2e4
임채리 <limch9eri@google.com>
2024-01-01 22:37:07 +0900

    feat: 상품 컴포넌트 구현

---
+++ src/components/mainpage/Item.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Item = () => {
+  return (
+    <ItemContainer>
+      <Num>1</Num>
+      <Img src="https://mall.ourhome.co.kr//attach_file/20201118/C4A1610E7468407FA935CBF8DE2164DF.jpg" />
+      <Name>★연말세일★ 진한 사골곰탕 300g</Name>
+      <Desc>100% 사골로 고은 진한 사골곰탕</Desc>
+      <Price>880원</Price>
+    </ItemContainer>
+  );
+};
+
+const ItemContainer = styled.div`
+  width: 284px;
+`;
+const Num = styled.div``;
+const Img = styled.img`
+  width: 284px;
+  height: 284px;
+  border: 1px solid #d9d9d9;
+`;
+const Name = styled.div``;
+const Desc = styled.div``;
+const Price = styled.div``;
+
+export default Item;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout/Layout';
+import Item from '../components/mainpage/Item';
 
 const MainPage = () => {
   return (
     <Layout>
+      <Title>카테고리 베스트</Title>
+      <Item />
     </Layout>
   );
 };
+
+const Title = styled.div``;
 
 export default MainPage;
